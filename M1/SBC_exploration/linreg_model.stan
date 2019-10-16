54dc1e44475e9c820a2af78e1d68b37280327ed8
Bryan Liu <liu.chbn@gmail.com>
2019-10-16 22:13:56 +0100

    SBC algorithm 1 seems to work now

--- M1/SBC_exploration/linreg_model.stan
+++ M1/SBC_exploration/linreg_model.stan
@@ -10,7 +10,7 @@ parameters {
 }
 
 model {
-  beta ~ normal(0, 10);
+  beta ~ normal(0, 0.5);
   alpha ~ normal(0, 10);
   
   y ~ normal(X * beta + alpha, 1.2);
--- M1/SBC_exploration/talts.R
+++ M1/SBC_exploration/talts.R
@@ -14,23 +14,39 @@ setwd('~/Imperial/statml/M1/SBC_exploration/')
 linreg_model <- stan_model(file='linreg_model.stan', 
                            model_name = "linreg_model")
 
-##### SBC Example
+M <- 20
+yyy <- parallel::mclapply(1:M, FUN = function(m) {
+  S <- seq(from = 0, to = .Machine$integer.max, length.out = M)[m]
+  
+  # Draw a prior sample
+  prior_samples <- list('beta'=rnorm(1, 0, 10), 'alpha'=rnorm(1, 0, 10))
+  
+  # Draw a simulated data
+  num_simulated_data <- 50
+  sample_data <- list(N=num_simulated_data,
+                      X=seq(0, num_simulated_data - 1),
+                      y=seq(0, num_simulated_data - 1) * prior_samples[['beta']] + 
+                        prior_samples[['alpha']] + 
+                        rnorm(num_simulated_data, 0, 1.2))
+  
+  # Run a MC for some iterations to get some posterior examples
+  post <- sampling(linreg_model, data=sample_data, chains = 1, cores = 1, 
+                   iter=3100, warmup=3000, seed = S)
+  
+  print(summary(post))
+  
+  # Compute ranks
+  ranks <- sapply(names(prior_samples), FUN = function(p){
+    posterior_samples <- extract(post)[[p]]
+    return(rank(append(posterior_samples, prior_samples[[p]]))[length(posterior_samples) + 1] - 1)
+  })
+  
+  return(ranks)
+})
 
-sbc_example <- stan_model(file='sbc.stan', model_name='sbc_example')
+yyy1 <- do.call(rbind, yyy)
 
-sbc_result <- sbc(sbc_example, data=list(N=100, a=0.1, b=0.2), M=100)
+hist(yyy1[,1], breaks=100)
+hist(yyy1[,2], breaks=100)
 
-plot(sbc_result)
-print(sbc_result)
-
-###### Linear regression model in Talts et al. (2018), changed to fit rstan::sbc
-
-linreg_model_sbc <- stan_model(file='linreg_model_sbc.stan', 
-                               model_name = "linreg_model_sbc")
-
-linreg_model_sbc_result <- sbc(
-  linreg_model_sbc, 
-  data=list(N=10, X=c(0, 1, 2, 3, 4.0, 5, 6, 7, 8, 9), 
-            y=c(-17.1, 10.39, -5.06, -7.94, -37.55, -46.18, 148.75, -46, -47, 39)), 
-  M=100)
 
